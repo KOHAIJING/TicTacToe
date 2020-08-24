9246c5100822c938d1c7e3f0a5c89d74d3edf71f
Koh Ai Jing <aj.koh@nettium.net>
2020-08-24 20:54:02 +0800

    solve bug

--- database/database.js
+++ database/database.js
@@ -1,8 +1,9 @@
+const env = require('dotenv').config();
 const mysql = require('mysql2');
 const dbConnection = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database : process.env.DB_NAME,
+  database: process.env.DB_NAME,
 }).promise();
 module.exports = dbConnection;
